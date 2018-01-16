5f091bf438a0d70ef4a2a42c29adc1f8c276ce6b
Tomasz Kępa <tomek@qed.ai>
2018-01-16 02:22:41 +0100

    Prepare for first stage of code generation

--- zad_02/src/CodeGen.hs
+++ zad_02/src/CodeGen.hs
@@ -5,17 +5,17 @@ import Data.Map as M
 import Data.Set as S
 
 import IRDef
-import Live
+import Live ( LiveMap, LiveState(..), LiveBlock(LB) )
 
 data CGReg = RDI | RSI | RDX | RCX | R8 | R9 | RAX | R10 | R11
-            | RBX | R12 | R13 | R14 | R15
+            | RBX | R12 | R13 | R14 | R15 | RBP
     deriving (Show, Eq, Ord)
 
-data CGMem = Reg CGReg | Mem CGReg Integer
+data CGMem = Reg CGReg | Mem CGReg Integer | Lit String
 
 data CGMachineState = CGMS {
     regToVar :: Map CGReg IRAddr,
-    varToMem :: Map IRAddr CGMem,
+    varToMem :: Map IRAddr (Set CGMem),
     nextInstrs :: [(IRInstr, LiveMap)],
     generatedCode :: [String]
 }
@@ -27,7 +27,8 @@ data CGState = CGS {
     nextStrLbl :: Integer,
     currentMs :: CGMachineState,
     blockToMs :: Map Label CGMachineState,
-    blockToInitialMs :: Map Label CGMachineState
+    blockToInitialMs :: Map Label CGMachineState,
+    nextStackLoc :: Integer
 }
 
 type CGMonad = State CGState
@@ -39,60 +40,169 @@ initialCGState :: LiveState -> [Label] -> CGState
 initialCGState (LS _ lm bl) ord = do
     let initMss = M.fromList $ Prelude.map (\lbl -> (lbl, initialMs (bl ! lbl))) ord
     let initMainMs = initMss ! "main"
-    CGS bl lm M.empty 0 initMainMs initMss M.empty
+    CGS bl lm M.empty 0 initMainMs initMss M.empty (-8)
 
 getBlock :: Label -> CGMonad LiveBlock
 getBlock lbl = do
-    CGS bls _ _ _ _ _ _ <- get
+    CGS bls _ _ _ _ _ _ _ <- get
     return $ bls ! lbl
 
 getLiveIn :: Label -> CGMonad (Set IRAddr)
 getLiveIn lbl = do
-    CGS _ lm _ _ _ _ _ <- get
+    CGS _ lm _ _ _ _ _ _ <- get
     return $ lm ! lbl
 
 freshStringLbl :: String -> CGMonad Label
 freshStringLbl str = do
-    CGS bl lm s2l nsl cms b2ms b2ims <- get
+    CGS bl lm s2l nsl cms b2ms b2ims nloc <- get
     let newLbl = "str_" ++ show nsl
-    put $ CGS bl lm (M.insert str newLbl s2l) (nsl+1) cms b2ms b2ims
+    put $ CGS bl lm (M.insert str newLbl s2l) (nsl+1) cms b2ms b2ims nloc
     return newLbl
 
 getStringLbl :: String -> CGMonad Label
 getStringLbl str = do
-    CGS _ _ s2l _ _ _ _ <- get
+    CGS _ _ s2l _ _ _ _ _ <- get
     case M.lookup str s2l of
         Nothing -> freshStringLbl str
         Just lbl -> return lbl
 
-setCurrentMs :: Label -> CGMonad ()
-setCurrentMs lbl = do
+getCurrentMs :: CGMonad CGMachineState
+getCurrentMs = do
+    CGS _ _ _ _ cms _ _ _ <- get
+    return cms
+
+changeCurrentMs :: Label -> CGMonad ()
+changeCurrentMs lbl = do
     nextCurrentMs <- getMs lbl
-    CGS lb lm s2l nsl _ b2ms b2ims <- get
-    put $ CGS lb lm s2l nsl nextCurrentMs b2ms b2ims
+    CGS lb lm s2l nsl _ b2ms b2ims nloc <- get
+    put $ CGS lb lm s2l nsl nextCurrentMs b2ms b2ims nloc
+
+setCurrentMs :: CGMachineState -> CGMonad ()
+setCurrentMs cms = do
+    CGS lb lm s2l nsl _ b2ms b2ims nloc <- get
+    put $ CGS lb lm s2l nsl cms b2ms b2ims nloc
+
+saveCurrentMs :: Label -> CGMonad ()
+saveCurrentMs lbl = do
+    cms <- getCurrentMs
+    setMs lbl cms
 
 getMs :: Label -> CGMonad CGMachineState
 getMs lbl = do
-    CGS _ _ _ _ _ b2ms _ <- get
+    CGS _ _ _ _ _ b2ms _ _ <- get
     return $ b2ms ! lbl
 
 setMs :: Label -> CGMachineState -> CGMonad ()
 setMs lbl ms = do
-    CGS lb lm s2l nsl cms b2ms b2ims <- get
-    put $ CGS lb lm s2l nsl cms (M.insert lbl ms b2ms) b2ims
+    CGS lb lm s2l nsl cms b2ms b2ims nloc <- get
+    put $ CGS lb lm s2l nsl cms (M.insert lbl ms b2ms) b2ims nloc
 
 getInitMs :: Label -> CGMonad CGMachineState
 getInitMs lbl = do
-    CGS _ _ _ _ _ _ b2ims <- get
+    CGS _ _ _ _ _ _ b2ims _<- get
     return $ b2ims ! lbl
 
 setInitMs :: Label -> CGMachineState -> CGMonad ()
 setInitMs lbl ms = do
-    CGS lb lm s2l nsl cms b2ms b2ims <- get
-    put $ CGS lb lm s2l nsl cms b2ms (M.insert lbl ms b2ims)
+    CGS lb lm s2l nsl cms b2ms b2ims nloc <- get
+    put $ CGS lb lm s2l nsl cms b2ms (M.insert lbl ms b2ims) nloc
+
+freshStackLoc :: CGMonad CGMem
+freshStackLoc = do
+    CGS lb lm s2l nsl cms b2ms b2ims nloc <- get
+    put $ CGS lb lm s2l nsl cms b2ms b2ims (nloc-8)
+    return $ Mem RBP nloc
+
+resetStackLoc :: CGMonad ()
+resetStackLoc = do
+    CGS lb lm s2l nsl cms b2ms b2ims _ <- get
+    put $ CGS lb lm s2l nsl cms b2ms b2ims (-8)    
+
+getLocSize :: CGMonad Integer
+getLocSize = do
+    CGS _ _ _ _ _ _ _ nloc <- get
+    return $ if nloc `mod` 16 == 8 then nloc + 8 else nloc
+
+addInstr :: String -> CGMonad ()
+addInstr instr = do
+    CGMS r2v v2m is gc <- getCurrentMs
+    setCurrentMs $ CGMS r2v v2m is (instr:gc)
+
+getNextInstr :: CGMonad (Maybe (IRInstr, LiveMap))
+getNextInstr = do
+    CGMS _ _ ni _ <- getCurrentMs
+    case ni of
+        (i:_) -> return $ Just i
+        _ -> return Nothing
+
+removeNextInstr :: CGMonad ()
+removeNextInstr = do
+    CGMS r2v v2m (i:is) gc <- getCurrentMs
+    setCurrentMs $ CGMS r2v v2m is gc
 
 
+getMemoryLoc :: IRAddr -> LiveMap -> LiveMap -> CGMonad CGMem
+getMemoryLoc var preLm postLm = case var of
+    ImmInt int -> return $ Lit $ "$" ++ show int
+    ImmString str -> do
+        lbl <- getStringLbl str
+        return $ Lit lbl
+    ImmBool b -> return $ Lit $ "$" ++ if b then "1" else "0"
+    NoRet -> return $ Lit "$0"
+    Indirect _ -> return $ Lit "$0" -- TODO
+
+
+genInstrs :: (IRInstr, LiveMap) -> CGMonad ()
+genInstrs (i, lm) = return ()
+    -- case i of
+        -- IRAss op dst larg rarg -> return ()
+
+
+firstPassOnBlock :: Label -> CGMonad ()
+firstPassOnBlock lbl = do
+    instr <- getNextInstr
+    case instr of 
+        Nothing -> return ()
+        Just i -> do
+            removeNextInstr
+            case i of
+                (IRIf {}, _)-> return ()
+                (IRGoto {}, _) -> return ()
+                _ -> do
+                    genInstrs i
+                    firstPassOnBlock lbl
+
+
+firstPassOnFunc :: Set Label -> Set Label -> CGMonad (Set Label)
+firstPassOnFunc next visited
+    | S.null next = return visited
+    | otherwise   = do
+        let lbl = S.elemAt 0 next
+        changeCurrentMs lbl
+        firstPassOnBlock lbl
+        saveCurrentMs lbl
+        LB _ _ nb _ <- getBlock lbl
+        let nbSet = S.fromList nb
+        let n1 = S.delete lbl next
+        let v1 = S.insert lbl visited
+        let n2 = S.union n1 (S.difference nbSet v1)
+        firstPassOnFunc n2 v1
+
+firstPass :: [Label] -> Set Label -> CGMonad ()
+firstPass [] _ = return ()
+firstPass (b:bs) visited 
+    | b `S.member` visited = firstPass bs visited
+    | otherwise            = do
+        resetStackLoc    
+        v1 <- firstPassOnFunc (S.singleton b) visited
+        -- TODO: add func prologue
+        firstPass bs v1
+
+
+genCode :: [Label] -> CGMonad [String]
+genCode ord = do
+    firstPass ord S.empty
+    return ["Hello"]
 
 generateAsm :: LiveState -> [Label] -> [String]
-generateAsm (LS _ li extb) blockOrder = do
-    ["hello"]
+generateAsm ls ord = evalState (genCode ord) (initialCGState ls ord)
